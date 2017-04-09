5814f14018052bdb51facab0db52e6a53f68c0b8
muyeby <bxf_hit@163.com>
2017-04-09 10:08:35 +0800

    update gitignore

--- Perceptron.py
+++ Perceptron.py
@@ -9,116 +9,149 @@
 @site: http://muyeby.github.io
 @software: PyCharm
 @file: Perceptron.py
-@time: 17-3-26 下午2:51
+@time: 17-4-8 下午4:21
 """
 import copy
-from matplotlib import pyplot as plt
-from matplotlib import animation
+import numpy as np
 
-training_set = [[(3, 3), 1], [(4, 3), 1], [(1, 1), -1]]
-w = [0, 0]
-b = 0
-eta = 1.0       #学习率
-history = []   #存放记录
-
-def update(item,eta):
-    '''
-    随机梯度下降法更新参数w(t+1)=w(t)+ eta*item[1]*item[0];b(t+1) = b(t)+ eta*item[1]
-    :param item: item的结构[(3, 3), 1],第一个元组是x,第二个是分类
-    :param eta: 学习率
-    :return: NULL
-    '''
-    global w,b,history
-    for i in range(len(w)):
-        w[i]+=eta*item[1]*item[0][i]
-    b += eta * item[1]
-    print w, b
-    history.append([copy.copy(w), b])
-
-def calc(item):
-    '''
-    计算 (w*x+b)*y
-    :param item: item的结构[(3, 3), 1],第一个元组是x,第二个是分类 
-    :return: (w*x+b)*y
-    '''
-    res =0
-    for i in range(len(item[0])):
-        res+=w[i]*item[0][i]
-    res+=b
-    res*=item[1]
-    return res
-
-
-def Judge():
-    flag = False
-    for item in training_set:
-        if calc(item) <= 0:
-            flag = True
-            update(item,eta)
-    if not flag:
-        print "Result: w = "+str(w)+" b = "+str(b)
-    return flag
-
-
-def draw():
-    '''
-    画出感知器参数调整的过程图
-    :return: 
-    '''
-    # 初始化
-    global line,label
-    fig = plt.figure()
-    ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
-    line, = ax.plot([], [], 'g', lw=2)
-    label = ax.text([], [], '')
-
-    def init():
-        line.set_data([], [])   #感知机超平面，这里是直线
-        x, y, x_, y_ = [], [], [], []   #两类点
-        for p in training_set:
-            if p[1] > 0:
-                x.append(p[0][0])
-                y.append(p[0][1])
-            else:
-                x_.append(p[0][0])
-                y_.append(p[0][1])
-
-        plt.plot(x, y, 'bo', x_, y_, 'rx')  #画点
-        plt.axis([-6, 6, -6, 6])    #数轴
-        plt.grid(True)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.title('Perceptron Algorithm')
-        return line, label
-
-    def animate(i):
+class BasePerceptron(object):
+    '''Class BasePerceptron'''
+    def __init__(self,learnRate):
         '''
-        每一帧的画面
-        :param i: 帧号
+        :param learnRate: 学习率
+        :param Dual: 是否使用对偶形式
         '''
-        global history, ax, line, label
+        self.learnRate = learnRate
+        self.History=[]
 
-        w = history[i][0]
-        b = history[i][1]
-        if w[1] == 0: return line, label
-        # 画线的两个端点
-        x1 = -7
-        y1 = -(b + w[0] * x1) / w[1]
-        x2 = 7
-        y2 = -(b + w[0] * x2) / w[1]
-        line.set_data([x1, x2], [y1, y2])
-        # label的坐标
-        x1 = 0
-        y1 = -(b + w[0] * x1) / w[1]
-        label.set_text(history[i])
-        label.set_position([x1, y1])
-        return line, label
+class Perceptron(BasePerceptron):
+    '''An implementation of Perceptron
+    Attributes:
+        trainData: 训练数据
+        learnRate: 感知器学习率
+    Results:
+        w:超平面参数w
+        b:超平面截距b
+        History: 超平面变动过程记录
+    '''
+    def train(self,trainData):
+        self.Data = trainData
+        self.y = np.array(trainData[:, 1])
+        self.x = np.empty((len(trainData), 2), np.float)
+        for i in range(len(trainData)):
+            self.x[i] = trainData[i][0]
+        self.w = np.zeros(len(self.x[0]), np.float)
+        self.b = 0.0
+        for i in range(1000):
+            if not self.Judge():
+                # print "Can't find a result within 1000 iterations!\n"
+                break
+        return self.w,self.b,self.History
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(history), interval=1000, repeat=True,
-                                   blit=True)
-    plt.show()
-    anim.save('perceptron.gif', fps=2, writer='imagemagick')
-if __name__ == "__main__":
-    for i in range(1000):
-        if not Judge(): break
-    draw()
+    def Judge(self):
+        flag = False
+        for i in range(len(self.Data)):
+            if self.calc(i) <= 0:
+                flag = True
+                self.update(i)
+        if not flag:
+            print "Result: w = " + str(self.w) + " b = " + str(self.b)
+        return flag
+
+    def calc(self,i):
+        '''
+        计算 (w*x+b)*y
+        :param i 第i组元素
+        :return: (w*x+b)*y
+        '''
+        res = np.dot(self.w,self.x[i])
+        res += self.b
+        res *= self.y[i]
+        return res
+    def update(self,i):
+        '''
+        更新超平面
+        w = w + eta*x[i]*y[i]
+        b = b+ eta*y[i]
+        :param i: 第i组元素
+        '''
+        self.w += self.learnRate * self.y[i]*self.x[i]
+        self.b += self.learnRate * self.y[i]
+        self.History.append([copy.copy(self.w), self.b])
+
+class DualPerceptron(BasePerceptron):
+    '''An implementation of Dual Perceptron 
+    Attributes:
+        trainData: 训练数据
+        learnRate: 感知器学习率
+    Results:
+        w:超平面参数w
+        b:超平面截距b
+        History: 超平面变动过程记录
+    '''
+    def train(self,trainData):
+        self.Data = trainData
+        self.y = np.array(trainData[:, 1])
+        self.x = np.empty((len(trainData), 2), np.float)
+        for i in range(len(trainData)):
+            self.x[i] = trainData[i][0]
+        self.n = np.zeros(len(trainData), np.float)
+        self.b = 0.0
+        self.Gram = self.calGram(self.Data)
+        for i in range(1000):
+            if not self.Judge():
+                # print "Can't find a result within 1000 iterations!\n"
+                break
+        a = self.learnRate * self.n
+        w = np.dot(a * self.y, self.x)
+        return w,self.b,self.History
+
+    def calGram(self,data):
+        """
+            calculate the Gram matrix
+            :return:
+            """
+        g = np.empty((len(data), len(data)), np.float)
+        for i in range(len(data)):
+            for j in range(len(data)):
+                g[i][j] = np.dot(data[i][0], data[j][0])
+        return g
+
+    def Judge(self):
+        '''
+        判断是否存在误分类的点
+        :return: boolean 
+        '''
+        flag = False
+        for i in range(len(self.Data)):
+            if self.calc(i) <= 0:
+                flag = True
+                self.update(i)
+        if not flag:
+            a = self.learnRate * self.n
+            w = np.dot(a * self.y, self.x)
+            b = np.dot(a, self.y)
+            print "Result: w = " + str(w) + " b = " + str(b)
+        return flag
+    def calc(self,i):
+        '''
+            计算y*(w*x+b)是否<=0
+            :param i: 
+            '''
+        a = self.learnRate * self.n
+        res = np.dot(a * self.y, self.Gram[i])
+        res = (res + self.b) * self.y[i]
+        return res
+
+    def update(self,i):
+        '''
+        更新操作
+        如果误分类，则该项的累加次数+1
+        :param i:第i个对象 
+        '''
+        global n, b
+        self.n[i] += 1
+        a = self.learnRate * self.n  # a = n*eta
+        self.b = np.dot(a, self.y)  # b = a*y
+        self.History.append([np.dot(a * self.y, self.x), self.b])  # w = a*y*x
+
---
+++ test_perceptron.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: muyeby
+@contact: bxf_hit@163.com
+@site: http://muyeby.github.io
+@software: PyCharm
+@file: test_perceptron.py
+@time: 17-4-8 下午5:25
+"""
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from Perceptron import Perceptron
+from Perceptron import DualPerceptron
+
+
+def showPic(history):
+
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+    line, = ax.plot([], [], 'g', lw=2)
+    label = ax.text([], [], '')
+
+    def init():
+        line.set_data([], [])
+        x, y, x_, y_ = [], [], [], []
+        for p in training_set:
+            if p[1] > 0:
+                x.append(p[0][0])
+                y.append(p[0][1])
+            else:
+                x_.append(p[0][0])
+                y_.append(p[0][1])
+
+        plt.plot(x, y, 'bo', x_, y_, 'rx')
+        plt.axis([-6, 6, -6, 6])
+        plt.grid(True)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('Perceptron Algorithm 2')
+        return line, label
+
+
+    def animate(i):
+
+        w = history[i][0]
+        b = history[i][1]
+        if w[1] == 0: return line, label
+        x1 = -7.0
+        y1 = -(b + w[0] * x1) / w[1]
+        x2 = 7.0
+        y2 = -(b + w[0] * x2) / w[1]
+        line.set_data([x1, x2], [y1, y2])
+        x1 = 0.0
+        y1 = -(b + w[0] * x1) / w[1]
+        label.set_text(str(history[i][0]) + ' ' + str(b))
+        label.set_position([x1, y1])
+        return line, label
+
+
+    # call the animator.  blit=true means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(history), interval=1000, repeat=True,
+                                   blit=True)
+    plt.show()
+    # anim.save('perceptron.gif', fps=2, writer='imagemagick')
+
+if __name__ == "__main__":
+    print "test Normal Perceptron\n"
+    training_set = np.array([[[3, 3], 1], [[4, 3], 1], [[1, 1], -1], [[5, 2], -1]])
+    per1 = Perceptron(1.0)
+    w,b,history=per1.train(training_set)
+    showPic(history)
+    print "test DualPerceptron\n"
+    training_set = np.array(([[[3, 3], 1], [[4, 3], 1], [[1, 1], -1]]))
+    per2 = DualPerceptron(1.0)
+    w2,b2,history2 = per2.train(training_set)
+    showPic(history2)
